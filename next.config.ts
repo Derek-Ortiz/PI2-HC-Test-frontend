f8104e480c010d9f7a60ac0534bd8d7fac24b961
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2026-03-04 21:24:06 -0600

    Standalone en next.comfig

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-  unoptimized: process.env.NODE_ENV === 'development',
-  remotePatterns: [
-    {
-      protocol: 'http',
-      hostname: 'localhost',
-      port: '3001',
-      pathname: '/uploads/**',
-    },
-  ],
-  
-},
+  output: "standalone",
+  images: {
+    unoptimized: process.env.NODE_ENV === 'development',
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
